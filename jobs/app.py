02608aeabc78eb0ed857871926e8c85011170d22
dievel01 <102391119+dievel01@users.noreply.github.com>
2022-03-26 13:10:54 -0300

    new updates

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+PATH = '/db/jobs.sqlite'
 
 app = Flask(__name__)
-
+def open.connection()
 
 @app.route('/')
 @app.route('/jobs')
